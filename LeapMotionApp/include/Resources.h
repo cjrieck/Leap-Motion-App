b82b990c5615a1d0c1db8d19b2c35a3f7ca36806
Bryan Jensen <jensen_bryan@wheatoncollege.edu>
2013-10-30 17:15:21 -0400

    Forgot to add the files

---
+++ LeapMotionApp/include/Resources.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+#define BLUE_FACE CINDER_RESOURCE(../../faces/, BlueFace.PNG, 128, IMAGE)
+
+
+
