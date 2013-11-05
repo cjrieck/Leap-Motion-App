4e55b8d2b1d397d18b8a9820aeec4cd165104dc2
Clayton Rieck <cjrieck123@gmail.com>
2013-11-05 14:13:41 -0500

    Added the XCode version of the app

---
+++ LeapMotionApp_XCode/xcode/build/LeapMotionApp_XCode.build/Debug/LeapMotionApp_XCode.build/Objects-normal/i386/LeapMotionApp_XCodeApp.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/Clayton/Desktop/Repos/Leap-Motion-App/LeapMotionApp_XCode/src/LeapMotionApp_XCodeApp.cpp \
+  ../../../../cinder/include/cinder/app/AppNative.h \
+  ../../../../cinder/include/cinder/gl/Texture.h \
+  /Users/Clayton/Desktop/Repos/Leap-Motion-App/LeapMotionApp_XCode/xcode/../include/Resources.h \
+  ../../../../cinder/include/cinder/CinderResources.h \
+  /Users/Clayton/Desktop/Repos/Leap-Motion-App/LeapMotionApp_XCode/src/../../leap/Leap.h \
+  /Users/Clayton/Desktop/Repos/Leap-Motion-App/LeapMotionApp_XCode/src/../../leap/LeapMath.h
